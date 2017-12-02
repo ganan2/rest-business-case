cbf22485e97e0618a5bbbe79c0a5cad1f177d76d
ganan2 <ganan2@uis.edu>
2017-12-02 17:01:39 -0600

    Updated Pseudo Rest Application

--- src/main/java/com/pseudoRest/PseudoRestApplication.java
+++ src/main/java/com/pseudoRest/PseudoRestApplication.java
@@ -1,9 +1,17 @@
 package com.pseudoRest;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan("com.pseudoRest")
+@EnableJpaRepositories(basePackages = {"com.pseudoRest,domain", "com.pseudoRest.repos"})
 public class PseudoRestApplication {
 
 	public static void main(String[] args) {
