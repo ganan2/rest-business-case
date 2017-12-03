26f65f5d91ee8a5056ef2ba2648935e5dfa601c1
ganan2 <ganan2@uis.edu>
2017-12-02 19:42:31 -0600

    Documented dev-tools

--- src/main/java/com/pseudoRest/PseudoRestApplication.java
+++ src/main/java/com/pseudoRest/PseudoRestApplication.java
@@ -15,6 +15,16 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class PseudoRestApplication {
 
 	public static void main(String[] args) {
+
+		/**
+		 *
+		 * Applications that use spring-boot-devtools will automatically restart whenever files on the classpath change.
+		 * This can be a useful feature when working in an IDE as it gives a very fast feedback loop for code changes.
+		 * By default, any entry on the classpath that points to a folder will be monitored for changes.
+		 * Note that certain resources such as static assets and view templates do not need to restart the application.
+		 *
+		 * https://docs.spring.io/spring-boot/docs/current/reference/html/using-boot-devtools.html
+		 */
 		System.setProperty("spring.devtools.restart.enabled", "true");
 		System.setProperty("spring.devtools.restart.exclude", "static/**,public/**");
 		SpringApplication.run(PseudoRestApplication.class, args);
